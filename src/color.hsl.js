ed1a7224a4b96b281a7deac386f3437658438111
gero3 <jeroen-876@hotmail.com>
2009-11-19 12:37:31 -0700

    implemented toHSL

--- src/color.hsl.js
+++ src/color.hsl.js
@@ -25,9 +25,25 @@ $.color.HSL = {
 	}
 };
 
-$.color.RGB.toHSL =
-	toHSL: function ( rgb ) {
-		// TODO
+$.color.RGB.toHSL =function ( rgb ) {
+	var r = rgb[0]/255,
+		g = rgb[1]/255,
+		b = rgb[2]/255,
+		min = Math.min(r,g,b),
+		max = Math.max(r,g,b),
+		d = max - min,
+		t = max + min,
+		l = t/2;
+ 
+	return [
+		d === 0 ? 0 :
+		(g === max ? (b-r)/d/6 + (1/3) :
+		 b === max ? (r-g)/d/6 + (2/3) :
+		             (g-b)/d/6 + 1) % 1,
+		d === 0 ? 0 : 
+                l <= .5 ? d/t :
+                d/(2-t),
+		l];
 	}
 });
 
